5ce864d0bd2397b10172cbc9804acdbfc8d9a42f
Jeffrey Rose <jtr219@outlook.com>
2021-02-11 18:29:14 -0500

    add explain-analyze test

--- src/resolvers/nycAddresses.ts
+++ src/resolvers/nycAddresses.ts
@@ -15,4 +15,16 @@ export class NYC_Addresses_Resolver {
 
     return res.rows.map((x) => x.street)
   }
+
+  @Query(() => [String])
+  async analyzeStreetsByName(@Arg('streetName') streetName: string) {
+    const res = await pool.query(sql`
+      EXPLAIN ANALYZE SELECT * FROM new_york_addresses
+      WHERE street = ${streetName};
+      `)
+
+    return res.rows.map((x) => JSON.stringify(x))
+    // CREATE INDEX street_idx ON new_york_addresses (street)
+    // indexes speed up search, good for WHERE columns and FK joins
+  }
 }
